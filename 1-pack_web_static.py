88305deda028667fdfab5d87b973ebe90d6a046a
Shaker Sharabi <sheko100@icloud.com>
2023-12-08 04:57:17 +0200

    Add 1-pack_web_static.py file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Fabric file to archive the web_static directory
+"""
+from fabric.operations import local
+import datetime
+
+
+def do_pack():
+    """generates a tgz archive from the contents of the web_static directory
+    """
+    d = datetime.datetime.now()
+    y = d.year
+    m = d.strftime("%m")
+    da = d.strftime("%d")
+    h = d.hour
+    mi = d.minute
+    sec = d.second
+
+    output_name = "web_static_{}{}{}{}{}{}.tgz".format(y, m, da, h, mi, d.sec)
+
+    local("test -d versions || mkdir versions")
+
+    try:
+        local("tar -cvzf versions/{} web_static".format(output_name))
+        return "versions/{}".format(output_name)
+    except err:
+        return None
