859b452a1e9ffb544946f4c0b6c9c44a8709536b
Shaker Sharabi <sheko100@icloud.com>
2023-12-28 05:50:15 +0200

    Fix config file name

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ echo -e "$html" > /data/web_static/releases/test/index.html
 test -d /data/web_static/current && rm /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-cfg_path="/etc/nginx/sites-enabled/default"
-hbnb_alias="\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current/index.html;\n\t\t}"
+cfg_path="/etc/nginx/sites-available/default"
+hbnb_alias="\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current/;\n\t\t}"
 grep -q "location /hbnb_static" "$cfg_path" || sed -i "s|^\tlocation / {|\tlocation / {\n\n$hbnb_alias|" "$cfg_path"
 service nginx reload
