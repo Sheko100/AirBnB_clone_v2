ff37ae2be4f717d3ede3731e013d7cc651701c30
Shaker Sharabi <sheko100@icloud.com>
2024-02-21 07:07:43 +0200

    Add storage switch

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,13 @@
 """Module to initialize variables
 """
 
-from models.engine.file_storage import FileStorage
+import os
 
-storage = FileStorage()
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
 storage.reload()
