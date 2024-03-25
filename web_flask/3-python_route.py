8215bb19e929be3b564912efc78bd20274649ae5
Shaker Sharabi <sheko100@icloud.com>
2024-03-25 03:41:57 +0200

    Add 3-python_route.py file

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module that creates a flask app
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greeting():
+    """ Generates 'Hello HBNB!' text on the root route
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Generates 'HBNB' text on the '/hbnb' route
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Generates text appended to 'C ' on the '/c/<text>' route
+    """
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is_cool"):
+    """ Generates text appended to 'Python ' if there is a text passed
+    otherwise 'Python is cool'on the '/python/(<text>)' route
+    """
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
